4b4adc127fffe5d944181f0d2e6baec3ac615a04
GevrieL <gevriel66@gmail.com>
2026-08-14 00:25:56 +0700

    perf: optimize Puppeteer launch flags & webVersionCache for low-resource VPS

--- src/services/whatsappService.js
+++ src/services/whatsappService.js
@@ -21,9 +21,21 @@ class WhatsAppService {
                 clientId: 'invoice-bot-session',
                 dataPath: path.resolve(process.cwd(), 'storage/wa_session')
             }),
+            webVersionCache: {
+                type: 'remote',
+                remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html'
+            },
             puppeteer: {
                 headless: true,
-                args: ['--no-sandbox', '--disable-setuid-sandbox']
+                args: [
+                    '--no-sandbox',
+                    '--disable-setuid-sandbox',
+                    '--disable-dev-shm-usage',
+                    '--disable-accelerated-2d-canvas',
+                    '--no-first-run',
+                    '--no-zygote',
+                    '--disable-gpu'
+                ]
             }
         });
 
